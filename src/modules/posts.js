54cedce50c71b1595e935bf0744eed397369d10f
Tapuwan <68889258+JeongTaehwan@users.noreply.github.com>
2021-07-11 16:02:24 +0900

    [ADD] redux-thunk로 Promise 다루기 - 3 Redux-Thunk 리펙토링

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -1,5 +1,5 @@
 import * as postsAPI from '../api/posts';
-import { reducerUtils } from '../lib/asyncUtils';
+import { createPromiseThunk, reducerUtils } from '../lib/asyncUtils';
 
 // 여러개 불러옴
 const GET_POSTS = 'GET_POSTS';
@@ -11,33 +11,8 @@ const GET_POST = 'GET_POST';
 const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
 const GET_POST_ERROR = 'GET_POST_ERROR';
 
-export const getPosts = () => async dispatch => {
-    // 요청이 시작됨
-    dispatch({ type: GET_POSTS });
-    // API를 호출
-    try {
-        const posts = await postsAPI.getPosts();
-        // 성공했을 때
-        dispatch({ type: GET_POSTS_SUCCESS, posts });
-    } catch (e) {
-        // 실패했을 때
-        dispatch({ type: GET_POSTS_ERROR, error: e });
-    }
-};
-
-export const getPost = (id) => async dispatch => {
-    // 요청이 시작됨
-    dispatch({ type: GET_POST });
-    // API를 호출
-    try {
-        const post = await postsAPI.getPosts(id);
-        // 성공했을 때
-        dispatch({ type: GET_POST_SUCCESS, post });
-    } catch (e) {
-        // 실패했을 때
-        dispatch({ type: GET_POST_ERROR, error: e });
-    }
-};
+export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
+export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
 
 // 초깃값
 const initialState = {
@@ -56,12 +31,12 @@ export default function posts(state = initialState, action) {
         case GET_POSTS_SUCCESS:
             return {
                 ...state,
-                posts: reducerUtils.success(action.posts)
+                posts: reducerUtils.success(action.payload)
             };
         case GET_POSTS_ERROR:
             return {
                 ...state,
-                posts: reducerUtils.error(action.error)
+                posts: reducerUtils.error(action.payload)
             };
         case GET_POST:
             return {
