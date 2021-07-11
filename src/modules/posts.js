a6c25823c72ee9bae5c631b9d8436c385f555202
Tapuwan <68889258+JeongTaehwan@users.noreply.github.com>
2021-07-11 15:34:32 +0900

    [ADD] redux-thunk로 Promise 다루기 - 2 Reducer 함수 리펙토링

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -1,4 +1,5 @@
 import * as postsAPI from '../api/posts';
+import { reducerUtils } from '../lib/asyncUtils';
 
 // 여러개 불러옴
 const GET_POSTS = 'GET_POSTS';
@@ -22,7 +23,7 @@ export const getPosts = () => async dispatch => {
         // 실패했을 때
         dispatch({ type: GET_POSTS_ERROR, error: e });
     }
-}
+};
 
 export const getPost = (id) => async dispatch => {
     // 요청이 시작됨
@@ -36,4 +37,48 @@ export const getPost = (id) => async dispatch => {
         // 실패했을 때
         dispatch({ type: GET_POST_ERROR, error: e });
     }
-}
+};
+
+// 초깃값
+const initialState = {
+    posts: reducerUtils.initial(),
+    post: reducerUtils.initial()
+};
+
+// reducer
+export default function posts(state = initialState, action) {
+    switch (action.type) {
+        case GET_POSTS:
+            return {
+                ...state,
+                posts: reducerUtils.loading()
+            };
+        case GET_POSTS_SUCCESS:
+            return {
+                ...state,
+                posts: reducerUtils.success(action.posts)
+            };
+        case GET_POSTS_ERROR:
+            return {
+                ...state,
+                posts: reducerUtils.error(action.error)
+            };
+        case GET_POST:
+            return {
+                ...state,
+                post: reducerUtils.loading()
+            };
+        case GET_POST_SUCCESS:
+            return {
+                ...state,
+                post: reducerUtils.success(action.post)
+            };
+        case GET_POST_ERROR:
+            return {
+                ...state,
+                post: reducerUtils.error(action.error)
+            };
+        default:
+            return state;
+    }
+};
