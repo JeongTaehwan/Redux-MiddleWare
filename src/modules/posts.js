4f6b72cc5dd1d23ed96929c693b8f866735e25a5
Tapuwan <68889258+JeongTaehwan@users.noreply.github.com>
2021-07-11 16:21:12 +0900

    [ADD] redux-thunk로 Promise 다루기 - 4 Reducer 함수 리펙토링 2

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -1,5 +1,5 @@
 import * as postsAPI from '../api/posts';
-import { createPromiseThunk, reducerUtils } from '../lib/asyncUtils';
+import { createPromiseThunk, handleAsyncActions, reducerUtils } from '../lib/asyncUtils';
 
 // 여러개 불러옴
 const GET_POSTS = 'GET_POSTS';
@@ -11,6 +11,7 @@ const GET_POST = 'GET_POST';
 const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
 const GET_POST_ERROR = 'GET_POST_ERROR';
 
+// Thunk 함수
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
 export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
 
@@ -20,39 +21,20 @@ const initialState = {
     post: reducerUtils.initial()
 };
 
+const getPostsReducer = handleAsyncActions(GET_POSTS, 'posts');
+const getPostReducer = handleAsyncActions(GET_POST, 'post');
+
 // reducer
 export default function posts(state = initialState, action) {
     switch (action.type) {
         case GET_POSTS:
-            return {
-                ...state,
-                posts: reducerUtils.loading()
-            };
         case GET_POSTS_SUCCESS:
-            return {
-                ...state,
-                posts: reducerUtils.success(action.payload)
-            };
         case GET_POSTS_ERROR:
-            return {
-                ...state,
-                posts: reducerUtils.error(action.payload)
-            };
+            return getPostsReducer(state, action);
         case GET_POST:
-            return {
-                ...state,
-                post: reducerUtils.loading()
-            };
         case GET_POST_SUCCESS:
-            return {
-                ...state,
-                post: reducerUtils.success(action.post)
-            };
         case GET_POST_ERROR:
-            return {
-                ...state,
-                post: reducerUtils.error(action.error)
-            };
+            return getPostReducer(state, action);
         default:
             return state;
     }
