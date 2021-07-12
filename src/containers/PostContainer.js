fc609c97bbab84c2a7d11a14c5fc6a92f88b8981
Tapuwan <68889258+JeongTaehwan@users.noreply.github.com>
2021-07-12 10:10:49 +0900

    [FIX] 오류해결

--- src/containers/PostContainer.js
+++ src/containers/PostContainer.js
@@ -1,23 +1,21 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import Post from '../components/Post';
 import { getPost } from '../modules/posts';
+import Post from '../components/Post';
 
 function PostContainer({ postId }) {
-    const { data, loading, error } = useSelector(state.posts.post);
+    const { data, loading, error } = useSelector(state => state.posts.post);
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(getPost(postId));
     }, [postId, dispatch]);
 
-    if (loading) return <div>로딩중..</div>
-    if (error) return <div>에러발생!</div>
+    if (loading) return <div>로딩중...</div>;
+    if (error) return <div>에러 발생!</div>;
     if (!data) return null;
 
-    return (
-        <Post />
-    );
+    return <Post post={data} />;
 }
 
 export default PostContainer;
